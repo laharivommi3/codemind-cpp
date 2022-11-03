33e1213c6f7afa6a914429a41eade5d6064dbce8
Lakshmi Prasanna Lahari Vommi <83864997+laharivommi3@users.noreply.github.com>
2022-11-03 22:56:23 +0530

    #include <bits/stdc++.h> using namespace std; int main() {     int n,rev=0,r;     cin>>n;     while(n>0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     cout<<rev; }

---
+++ reverse_of_a_numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,rev=0,r;
+    cin>>n;
+    while(n>0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    cout<<rev;
+}
